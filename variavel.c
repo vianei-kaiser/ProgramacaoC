328b903626c04b74771f6c9a1a0ce0e372352b2d
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-02-12 17:43:14 +0000

    modified:   olamundo 	new file:   variavel 	new file:   variavel.c

---
+++ variavel.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main () {
+
+    int idade = 25;
+    int quantidade =1;
+    float altura =1.75;
+    double peso=90.3;
+    char letra = 'A';
+    char nome[20] = "vianei";
+
+    idade=30;
+
+    printf("IDADE      = %d\n", idade);
+    printf("QUANTIDADE = %d\n", quantidade);
+    printf("ALTURA     = %.2f\n", altura);
+    printf("PESO       = %.1f\n", peso);
+    printf("LETRA      = %c\n", letra);
+    printf("NOME       = %s\n", nome);
+
+}
