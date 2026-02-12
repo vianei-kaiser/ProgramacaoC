01a4897485b72c5476895b1bdaf0a02566e23950
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-02-12 18:49:04 +0000

    modified:   variavel 	modified:   variavel.c

--- variavel.c
+++ variavel.c
@@ -11,11 +11,40 @@ int main () {
 
     idade=30;
 
+    printf("======================\n", nome);
     printf("IDADE      = %d  \n", idade);
     printf("QUANTIDADE = %d  \n", quantidade);
     printf("ALTURA     = %.2f\n", altura);
     printf("PESO       = %.1f\n", peso);
     printf("LETRA      = %c  \n", letra);
     printf("NOME       = %s  \n", nome);
+    printf("======================\n\n\n", nome);
+
+    printf("******* ENTRADA DE DADOS ********\n");
+
+    printf("Qual o seu nome: ");
+    scanf("%s", nome);   // NÃO usa & para string
+
+    printf("Qual a sua idade: ");
+    scanf("%d", &idade); // usa & para int
+
+    printf("Qual a sua altura: ");
+    scanf("%f", &altura); // usa & para float
+
+    printf("Qual o seu peso: ");
+    scanf("%lf", &peso); // usa & para double
+
+    printf("\n******* DADOS INFORMADOS ********\n");
+
+
+    printf("########## NOVOS DADOS ##########\n", nome);
+    printf("IDADE      = %d  \n", idade);
+    printf("QUANTIDADE = %d  \n", quantidade);
+    printf("ALTURA     = %.2f\n", altura);
+    printf("PESO       = %.1f\n", peso);
+    printf("LETRA      = %c  \n", letra);
+    printf("NOME       = %s  \n", nome);
+    printf("#################################\n\n\n", nome);
+
 
 }
